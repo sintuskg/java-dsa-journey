d1de39bea3427b1ae2ee002bff7b9813de504e89
sintuskg <119061183+sintuskg@users.noreply.github.com>
2026-08-09 03:00:37 +0530

    Update Search Insert Position solution format

--- 07-BinarySearch/LeetCode-35-Search-Insert-Position.java
+++ 07-BinarySearch/LeetCode-35-Search-Insert-Position.java
@@ -1,18 +1,49 @@
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
+/*
+ * LeetCode #35 - Search Insert Position
+ *
+ * Approach:
+ * Binary Search
+ *
+ * We use binary search because the array is sorted.
+ *
+ * If the target is found, return its index.
+ * If the target is not found, left will point to
+ * the correct position where the target should be inserted.
+ *
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ */
+
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
 
         while (left <= right) {
+
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target)
+            if (nums[mid] == target) {
                 return mid;
-            else if (nums[mid] < target)
+            }
+
+            if (nums[mid] < target) {
                 left = mid + 1;
-            else
+            } else {
                 right = mid - 1;
+            }
         }
 
         return left;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+
+        System.out.println(searchInsert(nums, target));
     }
 }
