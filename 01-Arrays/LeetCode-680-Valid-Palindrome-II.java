fc227d5136d1c0b38cf4b77fd4c023df258f6736
sintuskg <119061183+sintuskg@users.noreply.github.com>
2026-08-09 23:03:12 +0530

    feat: add LeetCode 680 valid palindrome II

---
+++ 01-Arrays/LeetCode-680-Valid-Palindrome-II.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean palindromeHelper(int i, int j, String s) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+
+            if (left != right) {
+                // Delete one element and check both possibilities.
+                return palindromeHelper(i + 1, j, s)
+                        || palindromeHelper(i, j - 1, s);
+            } else {
+                i++;
+                j--;
+            }
+        }
+
+        return true;
+    }
+}
