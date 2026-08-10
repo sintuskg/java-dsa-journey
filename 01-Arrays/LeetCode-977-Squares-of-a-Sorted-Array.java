22fc27c8133ff4aea416090494e471e740fec969
sintuskg <119061183+sintuskg@users.noreply.github.com>
2026-08-10 12:24:35 +0530

    docs: standardize LeetCode 977 Java format

--- 01-Arrays/LeetCode-977-Squares-of-a-Sorted-Array.java
+++ 01-Arrays/LeetCode-977-Squares-of-a-Sorted-Array.java
@@ -1,22 +1,54 @@
-class Solution {
-    public int[] sortedSquares(int[] nums) {
+/*
+ * LeetCode #977 - Squares of a Sorted Array
+ *
+ * Approach:
+ * Two Pointer
+ *
+ * We use two pointers:
+ * left  -> starts from the beginning
+ * right -> starts from the end
+ *
+ * The largest square can come from either end because
+ * the input array is sorted. We compare absolute values
+ * and place the larger square from the end of the result array.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+public class SquaresOfASortedArray {
+
+    public static int[] sortedSquares(int[] nums) {
+
         int[] res = new int[nums.length];
 
-        int i = 0;
-        int j = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
         int k = nums.length - 1;
 
-        while (i <= j) {
-            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
-                res[k] = nums[i] * nums[i];
-                i++;
+        while (left <= right) {
+
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                res[k] = nums[left] * nums[left];
+                left++;
             } else {
-                res[k] = nums[j] * nums[j];
-                j--;
+                res[k] = nums[right] * nums[right];
+                right--;
             }
+
             k--;
         }
 
         return res;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-4, -1, 0, 3, 10};
+        int[] result = sortedSquares(nums);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
     }
 }
