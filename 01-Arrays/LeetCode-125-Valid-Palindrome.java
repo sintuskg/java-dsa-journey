e05d35b9be389c0bbe25cf12d7e5339662cde21c
sintuskg <119061183+sintuskg@users.noreply.github.com>
2026-08-10 12:24:42 +0530

    docs: standardize LeetCode 125 Java format

--- 01-Arrays/LeetCode-125-Valid-Palindrome.java
+++ 01-Arrays/LeetCode-125-Valid-Palindrome.java
@@ -1,30 +1,61 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
+/*
+ * LeetCode #125 - Valid Palindrome
+ *
+ * Approach:
+ * Two Pointer
+ *
+ * We use two pointers:
+ * left  -> starts from the beginning
+ * right -> starts from the end
+ *
+ * We skip non-alphanumeric characters and compare
+ * characters after converting them to lowercase.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
 
-        while (i < j) {
-            char left = s.charAt(i);
-            char right = s.charAt(j);
+public class ValidPalindrome {
 
-            if (!Character.isLetterOrDigit(left)) {
-                i++;
+    public static boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            // Skip non-alphanumeric characters from the left
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
                 continue;
             }
 
-            if (!Character.isLetterOrDigit(right)) {
-                j--;
+            // Skip non-alphanumeric characters from the right
+            if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
                 continue;
             }
 
-            if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
+            // Compare characters without considering case
+            if (Character.toLowerCase(leftChar)
+                    != Character.toLowerCase(rightChar)) {
                 return false;
             }
 
-            i++;
-            j--;
+            left++;
+            right--;
         }
 
         return true;
+    }
+
+    public static void main(String[] args) {
+
+        String input = "A man, a plan, a canal: Panama";
+
+        System.out.println(isPalindrome(input));
     }
 }
